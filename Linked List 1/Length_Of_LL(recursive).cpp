e94d276a7a5100f30c883e9cf79351126e832212
Kishita Dhargawe <64804543+kish531@users.noreply.github.com>
2022-02-01 11:06:12 +0530

    Create Length_Of_LL(recursive).cpp

---
+++ Linked List 1/Length_Of_LL(recursive).cpp
@@ -0,0 +1,24 @@
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+int length(Node *head) {
+    if(head==NULL){
+        return 0;
+    }
+    return 1 + length(head->next);
+}
