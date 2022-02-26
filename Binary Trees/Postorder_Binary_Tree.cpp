c358b876ab31fa56c5d61219ce7511ba987720d7
Kishita Dhargawe <64804543+kish531@users.noreply.github.com>
2022-02-26 14:12:57 +0530

    Create Postorder_Binary_Tree.cpp

---
+++ Binary Trees/Postorder_Binary_Tree.cpp
@@ -0,0 +1,28 @@
+For a given Binary Tree of integers, print the post-order traversal.
+
+/**********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+void postOrder(BinaryTreeNode<int> *root) {
+    if(root==NULL){
+        return;
+    }postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
