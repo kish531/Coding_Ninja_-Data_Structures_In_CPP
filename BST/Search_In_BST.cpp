749d06d414e03c1ff0630fab067cd61df67881d3
Kishita Dhargawe <64804543+kish531@users.noreply.github.com>
2022-03-19 14:57:53 +0530

    Create Search_In_BST.cpp

---
+++ BST/Search_In_BST.cpp
@@ -0,0 +1,35 @@
+Given a BST and an integer k. Find if the integer k is present in given BST or not. You have to return true, if node with data k is present, return false otherwise.
+Note: Assume that BST contains all unique elements.
+
+/**********************************************************
+
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+bool searchInBST(BinaryTreeNode<int> *root , int k) {
+    if(root==NULL){
+        return false;
+    }
+    if(root->data==k){
+        return true;
+    }else if(root->data>k){
+        return searchInBST(root->left,k);
+    }else{
+        return searchInBST(root->right,k);
+    }
+}
